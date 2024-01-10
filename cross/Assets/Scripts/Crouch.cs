364f56fb96f6c907a0884bcd2f0bb5b99e312c87
Kat Pasalo <katpasalo@gmail.com>
2024-01-10 10:37:57 -0800

    Fix player launching on start

--- cross/Assets/Scripts/Crouch.cs
+++ cross/Assets/Scripts/Crouch.cs
@@ -10,8 +10,6 @@ public class Crouch : MonoBehaviour
     public float normalHeight = 4;
     public float crouchHeight = 1;
 
-    public Vector3 offset; // prevent falling through ground
-    public Transform player;
     bool isCrouching;
 
     void Update()
@@ -33,13 +31,6 @@ public class Crouch : MonoBehaviour
         float targetHeight = isCrouching ? crouchHeight : normalHeight;
         float smoothHeight = Mathf.Lerp(controller.height, targetHeight, crouchSpeed * Time.fixedDeltaTime);
 
-        // Smoothly adjust the controller height
         controller.height = smoothHeight;
-
-        float yOffset = (controller.height - normalHeight) / 2;
-        Vector3 targetPosition = new Vector3(player.position.x, normalHeight + yOffset, player.position.z);
-
-        // Smoothly adjust the player position
-        player.position = Vector3.Lerp(player.position, targetPosition, crouchSpeed * Time.smoothDeltaTime);
     }
 }
